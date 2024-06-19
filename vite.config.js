5dd0ec7b7ba53dbe2aa41a060647251590b0f266
rbd3 <rabedesanaandry01@gmail.com>
2024-06-19 06:24:51 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import babel from '@rollup/plugin-babel';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    babel({ 
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx'],
+      exclude: 'node_modules/**',
+    })],
+})
